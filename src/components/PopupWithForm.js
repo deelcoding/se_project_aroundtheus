9de796d8d432325f07f2554c883132830f047dbc
deelcoding <deelcoding@outlook.com>
2024-09-22 09:48:53 -0400

    Project 8 refactoring

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,17 @@
+import Popup from "./Popup.js"
+class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super({popupSelector});
+    this._popupForm = this._popupElement.querySelector(".modal_form");
+    this._handleFormSubmit = handleFormSubmit;
+  }
+
+  close() {
+    this._popupForm.reset();
+    super.close();
+  }
+}
+
+const newCardPopup = new PopupWithForm("#add-card-modal", () => {});
+newCardPopup.open();
+newCardPopup.close();
